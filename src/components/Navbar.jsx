21a81ba1e22943a34014349f0f452f51ab55a0de
Tzarin <135148480+Tzarin@users.noreply.github.com>
2023-08-13 18:16:16 -0400

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+
+
+export default function Navbar() {
+  return <>
+  <Nav>
+    <div className="brand">
+
+    </div>
+  </Nav>
+  
+  </>
+  
+}
